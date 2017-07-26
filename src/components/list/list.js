439d0695aabac8ba599f88d1253ccfb70b6975be
German Roman <German.Roman@wnco.com>
2017-07-26 16:21:20 -0300

    Todo List

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -6,11 +6,10 @@ import angular from 'angular';
 angular.module("myApp")
     .component("elementList", {
         template: require("./list.html"),
-        controller: ['elementList', function(elementList) {
-            
-            this.getList = function () {
-                console.log(elementList.getObj());
-            }
+        controller: ['elementList', function(elementList) {            
+            this.getList = () => {
+                this.elements = elementList.getArray();
+            };
         }]
     })
 
@@ -19,23 +18,7 @@ angular.module("myApp")
             'Elemento 0', 'Elemento 1', 'Elemento 2','Elemento 3' 
         ];
 
-        let objList = {
-            "elementos": {
-                objeto: 0,
-                objeto: 1,
-                objeto: 3,
-                objeto: 4,
-            }
-        };
-
         this.getArray = function () {
             return arrayList;
-        }
-
-        this.getObj = function () {
-            return objList;
-        }
-
-
-
+        };
     })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-angular.module("myApp", [])
-
+angular.module("myApp", []);
 require('./components');
 
     
